d93fc9abf8da1351b6f51e140d2df6e6672fb5b5
zhangjinning <zjnblank@gmail.com>
2021-03-14 13:31:25 +0800

    Initial commit

---
+++ src/main/java/top/zjnblank/Carnation/CarnationApplication.java
@@ -0,0 +1,13 @@
+package top.zjnblank.Carnation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/zjnblank/Carnation/CarnationApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zjnblank.Carnation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
